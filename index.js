b91736fada8a892308f6c5a68a23ff5f58ce5ac1
Phyuthazinthant <phyuthazin.mg@gmail.com>
2019-05-17 13:30:36 +0630

    gold commit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-	
 $('#smt').click(function(){
 	var a=parseInt($('#gp').val());
 	var b=parseInt($('#kt').val());
@@ -8,29 +7,46 @@ $('#smt').click(function(){
 	var d=parseInt($('#yal').val());
 	var f=parseInt($('#yal2').val());
 	var g=parseInt($('#lk').val());
-	var tpal=0, tyal=0;
-	var onek=128;
-	tpal=c+e;
-	tyal=d+f;
-if(tpal>=16 ) {
-var ptotal=tpal%16;
-	var kyt;
-	kyt=parseInt(tpal/16);
-var newkt=parseInt(kyt+b);
-}else if(tyal>=8 ){
- var ytotal=tyal%8;
-var newpl=parseInt(tyal/8);
-alert (newpl);
-var oneYal=parseInt((newpl*8)+ytotal));
-alert (oneYal);
-}
-else {
-	var oneYal=parseInt((tpal*8)+tyal);
-	var total=parseInt((oneYal/128)*a+g);
-alert ("The total number of gold price is:&nbsp;"+total);
-}
+	
+    var tpal=c+e;
+    var tyal=d+f;
 
+if(tyal>=8)
+{
+var ytotal=parseInt(tyal%8);
+var newpl=parseInt(tyal/8);
+var npal=parseInt(newpl+tpal);
+if(npal>=16)
+ {
+   var ptotal=parseInt(npal%16);
+   var kyt=parseInt(npal/16);
+   var newkt=parseInt(kyt+b);
+   var oneYal=parseInt((ptotal*8)+ytotal);
+   var result=parseInt(((oneYal/128)*a)+(a*newkt)+g);
+  $('#dv').html("The total number of gold price is "+"\""+result+"\"");
+ }
+else if(npal<16)
+  {
+    var oneYal=parseInt((npal*8)+ytotal);
+    var result=parseInt(((oneYal/128)*a)+(a*b)+g);
+	$('#dv').html("The total number of gold price is "+"\""+result+"\"");
+   }
+}
+else if(tpal>=16)
+  {
+	var ptotal=parseInt(tpal%16);
+    var kyt=parseInt(tpal/16);
+    var newkt=parseInt(kyt+b);
+	var oneYal=parseInt((ptotal*8)+tyal);
+	var result=parseInt(((oneYal/128)*a)+(a*newkt)+g);
+   //alert ("The total number of gold price is:\n"+total);
+   $('#dv').html("The total number of gold price is "+"\""+result+"\"");
+   }else 
+  {
+	var oneYal=parseInt((tpal*8)+tyal);
+	var result=parseInt(((oneYal/128)*a)+(a*b)+g);
+	//alert ("The total number of gold price is:\n"+total);
+    $('#dv').html("The total number of gold price is "+"\""+result+"\"");
+   }
 });
 });
-/**/
-	//alert ("gold weight is"+" "+a+"\n"+"kyatthar"+b);
